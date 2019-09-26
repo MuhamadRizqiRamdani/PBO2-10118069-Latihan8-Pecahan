eccb6c730f7b311d922535ba5a5a33f077d98330
Muhamad Rizqi Ramdani <55569266+MuhamadRizqiRamdani@users.noreply.github.com>
2019-09-26 16:53:29 +0700

    Add files via upload

---
+++ src/main/java/com/mycompany/pbo2/latihan8/pecahan/PBO210118069Latihan8Pecahan.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.pbo2.latihan8.pecahan;
+
+/**
+ *
+ * @author
+ * NAMA     : Muhamad Rizqi Ramdani
+ * KELAS    : PBO2
+ * NIM      : 10118069
+ * Deskripsi program : Program ini berisi program untuk hasil pecahan 
+ */
+public class PBO210118069Latihan8Pecahan {
+    public static void main(String[] args) {
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float : " + f);
+        System.out.println("Double : " + d);
+    }
+    
+}
